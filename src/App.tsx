26897683d6f4369575315460fd5e60f3dfbb0384
Piotr Szcześniak <piotr.szcz3sniak@gmail.com>
2023-02-04 23:37:02 +0100

    add gh-pages setting for an app using routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Products } from './pages/Products/Products';
 import { ErrorPage } from './pages/ErrorPage';
